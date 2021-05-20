d84a4fae32d36586ec80ca0ef76809f82f076f62
Gerald Phan <38089876+phange@users.noreply.github.com>
2021-05-19 23:33:11 -0500

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_phang',
-  password        : '8947',
-  database        : 'cs340_phang'
-});
-module.exports.pool = pool;
