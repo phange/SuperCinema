37628ac0ce42cc512a74c5c2b76f1ad5a7d34756
Gerald Phan <38089876+phange@users.noreply.github.com>
2021-05-19 23:32:21 -0500

    Delete public directory

--- public/filterpeople.js
+++
@@ -1,6 +0,0 @@
-function filterPeopleByHomeworld() {
-    //get the id of the selected homeworld from the filter dropdown
-    var homeworld_id = document.getElementById('homeworld_filter').value
-    //construct the URL and redirect to it
-    window.location = '/people/filter/' + parseInt(homeworld_id)
-}
