b782a821f641064baf18399d679e4f14b50e4e16
kasowskc <72176947+kasowskc@users.noreply.github.com>
2021-06-07 19:04:24 -0700

    Update to mySQL files
    - Added additional comments to better explain queries.

--- dmq.sql
+++ dmq.sql
@@ -5,16 +5,22 @@
 -- : = variable
 
 
--- Genres PAge
+-- Genres Page ------------------------------------------------------------------------------------------------------------------------------------------------
+-- displays Genres as is
 SELECT genreID, genreName from Genres;
 
+-- Basic insert for Genres table
 INSERT INTO Genres (genreName) VALUES (:genreName);
 
+-- Basic update for Genres table
 UPDATE Genres SET
     genreName = :genreName
+
+-- Delete from Genres table based on genreID
+DELETE FROM Genres WHERE genreID = ?
 ;
 
--- Movies Page
+-- Movies Page ------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- displays Movies as is
 SELECT Movies.movieID as id, movieTitle, genreID, movieDuration, movieRestriction, movieDescription FROM Movies;
@@ -24,11 +30,11 @@ SELECT Movies.movieID as id, movieTitle, Genres.genreName AS genreID, movieDurat
 -- AND matching Genre from filter dropdown
 SELECT Movies.movieID as id, movieTitle, Genres.genreName AS genreID, movieDuration, movieRestriction, movieDescription FROM Movies INNER JOIN Genres ON Movies.genreID = Genres.genreID WHERE Movies.genreID = ?;
 
--- basic insert
+-- Basic insert
 INSERT INTO Movies (movieTitle, genreID, movieDuration, movieRestriction, movieDescription)
 VALUES (:movieTitle, :genreID, :movieDuration, :movieRestriction, :movieDescription);
 
--- basic update
+-- basic update based on movieID
 UPDATE Movies SET 
     movieTitle = :movieTitle,
     genreID = :genreID,
@@ -37,32 +43,40 @@ UPDATE Movies SET
     WHERE movieID = :movieID
 ;
 
+-- Delete from movies table based on movieID
 DELETE FROM Movies WHERE movieID = :movieID;
 
 
--- Customers Page
+-- Customers Page ---------------------------------------------------------------------------------------------------------------------------------------------
 
+-- Display Customers as is
 SELECT * FROM Customers;
 
+-- Basic insert to Customers table
 INSERT INTO Customers (customerType, customerEmail)
 VALUES (:customerType, :customerEmail);
 
+-- Basic update to Customers table based on customerID
 UPDATE Customers SET
     customerType = :customerType,
     customerEmail = :customerEmail
     WHERE customerID = :customerID
 ;
 
-DELETE FROM Customers WHERE customerID = :customerID;
+-- Delete from Customers table based on customerID
+DELETE FROM Customers WHERE customerID = :customerID;  
 
 
--- Showings Page
+-- Showings Page ----------------------------------------------------------------------------------------------------------------------------------------------
 
+-- Display Showings as is
 SELECT * FROM Showings;
 
+-- Basic insert to Showings table
 INSERT INTO Showings (movieID, roomID, startTime, endTime, startDate, endDate, capacity)
 VALUES (:movieID, :roomID, :startTime, :endTime :startDate, :endDate, :capacity);
 
+-- Basic update to showings table
 UPDATE Showings SET 
     movieID = :movieID,
     roomID = :roomID,
@@ -72,15 +86,19 @@ UPDATE Showings SET
     endDate = :endDate
 ;
 
+-- Delete from Showings table based on showingID
 DELETE FROM Showings WHERE showingID = :showingID;
 
--- Ticket Purchases Page
+-- Ticket Purchases Page --------------------------------------------------------------------------------------------------------------------------------------
 
+-- Display Ticket_Purchases table as is
 SELECT * FROM Ticket_Purchases;
 
+-- Basic insert into Ticket_Purchases table
 INSERT INTO Ticket_Purchases (customerID, movieID, showingID, roomID, ticketPrice)
 VALUES (:customerID, :movieID, :showingID, :roomID, :ticketPrice);
 
+-- Basic update to Ticket_Purchases
 UPDATE Ticket_Purchases SET
     customerID = :customerID,
     movieID = :movieID,
@@ -89,6 +107,7 @@ UPDATE Ticket_Purchases SET
     ticketPrice = :ticketPrice
 ;
 
+-- Delete from Ticket_Purchases based on showingID
 DELETE FROM Ticket_Purchases WHERE showingID = :showingID; 
 
 
